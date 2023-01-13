c48f1f131b119407037d1b35733eacbb7805c251
lirui <lir2019@163.com>
2023-01-13 09:57:01 +0800

    rm test for tests

--- test.sh
+++ test.sh
@@ -8,7 +8,6 @@ rm build -r
 log=tmp_log
 
 ./build/test_configure_file 2>&1 | tee $log
-./build/test_test 2>&1 | tee -a $log
 ./build/test_lexer 2>&1 | tee -a $log
 echo ====================== failed cases ========================
 grep failure $log
