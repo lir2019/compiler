aabf32581f8edf27c9f0bce94c1f73542c0bf1e0
lirui <lir2019@163.com>
2023-01-22 19:30:05 +0800

    upgrade test kits

--- test/utils.hpp
+++ test/utils.hpp
@@ -8,8 +8,24 @@
 template <typename T>
 bool Test(const std::string &name,
           T out,
+          T expected) {
+  std::cout << name << " test result: ";
+  bool ret = false;
+  if (out == expected) {
+    std::cout << "success" << std::endl;
+    ret = true;
+  } else {
+    std::cout << "failure" << std::endl;
+    ret = false;
+  }
+  return ret;
+}
+
+template <typename T, typename PRINTER_TYPE>
+bool Test(const std::string &name,
+          T out,
           T expected,
-          void (*print)(T) = nullptr) {
+          PRINTER_TYPE print) {
   std::cout << name << " test result: ";
   bool ret = false;
   if (out == expected) {
@@ -32,8 +48,26 @@ template <typename IN_TYPE, typename OUT_TYPE>
 bool Test(const std::string &name,
           OUT_TYPE (*func)(IN_TYPE),
           IN_TYPE in,
+          OUT_TYPE expected) {
+  std::cout << name << " test result: ";
+  bool ret     = false;
+  OUT_TYPE out = func(in);
+  if (out == expected) {
+    std::cout << "success" << std::endl;
+    ret = true;
+  } else {
+    std::cout << "failure" << std::endl;
+    ret = false;
+  }
+  return ret;
+}
+
+template <typename IN_TYPE, typename OUT_TYPE, typename PRINTER_TYPE>
+bool Test(const std::string &name,
+          OUT_TYPE (*func)(IN_TYPE),
+          IN_TYPE in,
           OUT_TYPE expected,
-          void (*print)(OUT_TYPE) = nullptr) {
+          PRINTER_TYPE print) {
   std::cout << name << " test result: ";
   bool ret     = false;
   OUT_TYPE out = func(in);
